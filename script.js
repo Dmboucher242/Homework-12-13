be34d0c6055c481120c0e89716368cc9aebf1016
Dmboucher242 <58062008+Dmboucher242@users.noreply.github.com>
2019-12-12 21:02:46 -0700

    button changes

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+var generate = document.getElementById("generateBtn");
+generateBtn.addEventListener("click", function(){
+
+    console.log("complexity");
+});
+
+
+
 function generate(){
-    let complexity = document.getElementById("slidebar").values;
+    
+    // let complexity = document.getElementById("slidebar").value;
+    let complexity =42;
     let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_+-=";
     let password = "";
-
-    for(var i = 0; i <= complexity; i++){
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-
+  
+    for(var i = 0; i < complexity; i++){
+        password = password + values.charAt(Math.floor(Math.random() * (values.length)));
+        
     }
-    document.getElementById("textbox").values = password;
+    document.getElementById("textbox").textContent = password;
+    
 
 }
+
+
+
+
+
