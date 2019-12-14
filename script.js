a564764b1b0953a5478296f442af1423a4a80fa5
Dmboucher242 <58062008+Dmboucher242@users.noreply.github.com>
2019-12-13 18:13:39 -0700

    additions to the slidebar

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
+let complexity = prompt("Choose a password length between 8 and 128 characters long")
+   if (complexity < 8 || complexity >128){
+       alert("Must choose a number between 8 and 128");
+        complexity = prompt("Choose a password length between 8 and 128 characters long")
+ 
+   }
+       alert(complexity);
+let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_+-=";
 var generate = document.getElementById("generateBtn");
 generateBtn.addEventListener("click", function(){
-
-    console.log("complexity");
+ 
+   console.log(generate(complexity));
 });
-
-
-
-function generate(){
-    
-    // let complexity = document.getElementById("slidebar").value;
-    let complexity =42;
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_+-=";
-    let password = "";
+ 
+ 
+ 
+function generate(complexity){
   
+   let complexity = document.getElementById("slidebar").value;
+   let password = "";
     for(var i = 0; i < complexity; i++){
-        password = password + values.charAt(Math.floor(Math.random() * (values.length)));
-        
-    }
-    document.getElementById("textbox").textContent = password;
-    
-
+       password = values.charAt(Math.floor(Math.random() * (values.length)));
+       return password;
+   }
+   document.getElementById("textbox").textContent = password;
 }
-
-
-
-
-
