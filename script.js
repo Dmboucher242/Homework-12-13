574721a79b6471d6a269fb6d00dd007b7ce52e03
Dmboucher242 <58062008+Dmboucher242@users.noreply.github.com>
2020-01-04 23:00:32 -0700

    adding prompts and confirms to java

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-let complexity = prompt("Choose a password length between 8 and 128 characters long")
+var lowerCasedLetters = ["a","b","c","d","e","f","g","h", "i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCasedLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = [ "@","%","+","\\","/","'","!","#","$","^","?",":",",",")","(","}","{","]","[","~","-","_","."];
+
+
+
+
+   let complexity = prompt("Choose a password length between 8 and 128 characters long")
    if (complexity < 8 || complexity >128){
-       alert("Must choose a number between 8 and 128");
-        complexity = prompt("Choose a password length between 8 and 128 characters long")
- 
+      alert("Must choose a number between 8 and 128");
+      complexity = prompt("Choose a password length between 8 and 128 characters long")
+      
    }
-       alert(complexity);
-let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_+-=";
-var generate = document.getElementById("generateBtn");
-generateBtn.addEventListener("click", function(){
+   var lowerCasedLetters = confirm("Would you like your password to contain lower cased letters?");
+   var upperCasedLetters = confirm("Would you like your password to contain upper cased letters?");
+   var numbers = confirm("Would you like your password to contain numbers?")
+   var specialCharacters = confirm("Would you like your password to contain special characters?");
+
+   
+
+
  
-   console.log(generate(complexity));
-});
+   
+
+
+
+
  
  
- 
-function generate(complexity){
-  
-   let complexity = document.getElementById("slidebar").value;
-   let password = "";
-    for(var i = 0; i < complexity; i++){
-       password = values.charAt(Math.floor(Math.random() * (values.length)));
-       return password;
-   }
-   document.getElementById("textbox").textContent = password;
-}
